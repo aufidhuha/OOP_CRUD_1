115353a5533e8b4dbb79c48b0c064448a822a802
aufidhuha <aufikawanit@gmail.com>
2025-10-21 08:54:56 +0700

    first commit

---
+++ src/kelas/user.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kelas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class user {
+    
+    private String userName, userEmail, userPassword, userFullName;
+    private byte userStatus;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserFullName() {
+        return userFullName;
+    }
+
+    public void setUserFullName(String userFullName) {
+        this.userFullName = userFullName;
+    }
+
+    public byte getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(byte userStatus) {
+        this.userStatus = userStatus;
+    }
+    
+    
+    
+    
+}
